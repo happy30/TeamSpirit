7950b3019904fd2ba9740b3bf5ff7dfc44cff14d
Addamas <kingosman6@gmail.com>
2017-01-19 13:57:57 +0100

    level 2 chase almost done

---
+++ HanzakiProject/Assets/Scripts/Environment/ChaseMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseMover : MonoBehaviour {
+
+
+    public float moveX;
+    public float moveY;
+    public float moveZ;
+
+	void Update () {
+        transform.Translate(moveX, moveY, moveZ);
+	}
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerController>().Die();
+        }
+    }
+}
